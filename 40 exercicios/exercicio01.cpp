af71f34c17cea55e25794e72529a5b4469e69024
Karla C. Ferreira <106109272+kfrural@users.noreply.github.com>
2023-04-05 14:44:22 -0300

    Update exercicio01.cpp

--- 40 exercicios/exercicio01.cpp
+++ 40 exercicios/exercicio01.cpp
@@ -1,9 +1,17 @@
 #include <iostream>
 
 
+
 int main(){
 
     std::cout<<"Olá mundo" << std::endl;
+     std:: cout<<"Qual sua idade: " << std:: endl;
+    std:: cin>> idade;
+    std:: cout<<"Qual seu peso: " << std:: endl;
+    std:: cin>> peso;
+    std:: cout<< "Qual sua altura: " << std:: endl;
+    std:: cin>> altura;
+    std:: cout<< "Idade: "<<idade<< ", Altura: "<< altura<< ", Peso: "<<peso << std:: endl;
 
     return 0;
 }
