c6489da2c24b7b21dd0fe3f21183d3001febe88c
Karla C. Ferreira <106109272+kfrural@users.noreply.github.com>
2023-04-05 15:04:35 -0300

    Update exercicio02.cpp

--- 40 exercicios/exercicio02.cpp
+++ 40 exercicios/exercicio02.cpp
@@ -1,17 +1,27 @@
 #include <iostream>
-int idade=0;
-int peso=0;
-float altura=0;
+
+
 
 int main()
 {
-    std:: cout<<"Qual sua idade: " << std:: endl;
-    std:: cin>> idade;
-    std:: cout<<"Qual seu peso: " << std:: endl;
-    std:: cin>> peso;
-    std:: cout<< "Qual sua altura: " << std:: endl;
-    std:: cin>> altura;
-    std:: cout<< "Idade: "<<idade<< ", Altura: "<< altura<< ", Peso: "<<peso << std:: endl;
+   std:: cout<<"*" << std:: endl;
+   std:: cout<<"**" <<std:: endl;
+   std:: cout<<"***" <<std:: endl;
+   std:: cout<<"****" <<std:: endl;
+   std:: cout<<"\n\n\n" <<std:: endl;
+   
+   
+   std:: cout<<"****" << std:: endl;
+   std:: cout<<"***" <<std:: endl;
+   std:: cout<<"**" <<std:: endl;
+   std:: cout<<"*" <<std:: endl;
+   std:: cout<<"\n\n\n" <<std:: endl;
+   
+   
+    std:: cout<<"   *" << std:: endl;
+   std:: cout<<"  **" <<std:: endl;
+   std:: cout<<" ****" <<std:: endl;
+   std:: cout<<"******" <<std:: endl;
 
     return 0;
 }
