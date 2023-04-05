bd538054243747dd9442fa769cfc54a6fe11d4a2
Karla C. Ferreira <106109272+kfrural@users.noreply.github.com>
2023-04-05 15:08:54 -0300

    Update exercicio04.cpp

--- 40 exercicios/exercicio04.cpp
+++ 40 exercicios/exercicio04.cpp
@@ -1,1 +1,22 @@
 #include<iostream>
+using namespace std;
+
+int soma(int x, int y){
+		int resultado = x + y;
+		return resultado;
+}
+
+int main(){
+	int a = 0.0;
+	int b = 0.0;
+
+	cout << "Informe a:";
+	cin >> a;
+	cout << "Informe b:";
+	cin >> b;
+
+	int resposta = soma(a, b);
+	cout << "A soma de a e b é:" << resposta << endl;
+
+	return 0;
+}
