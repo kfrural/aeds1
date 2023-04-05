ff8dcac4811e7d5e267052660848b4787784e7d8
Karla C. Ferreira <106109272+kfrural@users.noreply.github.com>
2023-04-05 14:50:49 -0300

    Update exercicio01.cpp

--- 40 exercicios/exercicio01.cpp
+++ 40 exercicios/exercicio01.cpp
@@ -1,10 +1,17 @@
 #include <iostream>
 
-
+int idade=0;
+float peso=0;
+float altura=0;
+float motot=0;
+float eixo=0;
+float peso=0;
 
 int main(){
 
     std::cout<<"Olá mundo" << std::endl;
+    
+    
      std:: cout<<"Qual sua idade: " << std:: endl;
     std:: cin>> idade;
     std:: cout<<"Qual seu peso: " << std:: endl;
@@ -12,6 +19,16 @@ int main(){
     std:: cout<< "Qual sua altura: " << std:: endl;
     std:: cin>> altura;
     std:: cout<< "Idade: "<<idade<< ", Altura: "<< altura<< ", Peso: "<<peso << std:: endl;
-
+    
+    
+    std:: cout<<"Qual o motor: "<<std::endl;
+    std:: cin>> motor;
+    std:: cout<<"Qual o eixo: " << std:: endl;
+    std:: cin>> eixo;
+    std:: cout<<"Qual o peso: " << std:: endl;
+    std:: cout<< "Motor: "<< motor <<"\n eixos: "<< eixo << "\n Peso: "<< peso << std:: endl;
+    
+    
+   
     return 0;
 }
