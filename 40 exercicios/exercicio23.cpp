8d8ba9a6ae2063d8b60fca77a4a39057b222017a
Karla C. Ferreira <106109272+kfrural@users.noreply.github.com>
2023-04-12 15:06:00 -0300

    Create exercicio23.cpp

---
+++ 40 exercicios/exercicio23.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+
+using namespace std;
+
+bool par(int a, int b) {
+    if (a % 2 == 0 && b % 2 == 0) {
+        return true;
+    } else {
+        return false;
+    }
+    
+}
+
+int main() {
+    int n1 = 0;
+    int n2 = 0;
+    
+        cout<<"Digite um numero: ";
+        cin>>n1;
+        cout<<"Digite outro numero: ";
+        cin>>n2;
+        
+        if(par(n1,n2)){
+        cout<<"Os dois sao pares";
+        }else{
+        cout<<"Há ao menos um numero impar";
+        }
+        par(n1,n2);
+
+    
+    return 0;
+}
